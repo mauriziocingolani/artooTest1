30d2049f3bb140483bcf853d9ecad4dd4e2b7ba9
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2015-10-19 17:05:32 +0200

    Test2

--- index.php
+++ index.php
@@ -42,5 +42,10 @@ $config = array(
     ),
 );
 
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+
 include_once($yii);
 Yii::createWebApplication($config)->run();
